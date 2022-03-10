3a299813ef1b7e472a890a70e2dbf197127e3fd2
Yixuan <56627720+yixuanwu4@users.noreply.github.com>
2022-03-10 12:02:52 +0100

    fixed mistakes in train.csv

--- Using_LIAR/tmp.py
+++ Using_LIAR/tmp.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import glob
+import csv
 
 # convert all tsv files into csv file
-path = '/home/nauxiy/Workspace/reproNLP/SentimentalLIAR/Using_LIAR'
+path = '/home/nauxiy/Workspace/reproNLP/ReproductionProject/Using_LIAR'
 
 tsvfiles = glob.glob(path + "/*.tsv") 
 for t in tsvfiles:
@@ -17,3 +18,29 @@ for c in csvfiles:
     # converting data frame to csv
     file.to_csv(c, header=headerList, index=False)
 
+# add a new column to indicate if the text is fake or not (label each row as 1 or 0)
+destpath = '/home/nauxiy/Workspace/reproNLP/ReproductionProject/Using_LIAR/modified'
+destfiles = glob.glob(destpath + "/*.csv")
+falselist = ['half-true', 'false', 'barely-true', 'pants-fire' ]
+for c,d in zip(csvfiles, destfiles):
+    print(c,d)
+    with open(c, "rb") as source, open(d, "wb") as dest:
+        # XXX are you sure you want this as quotechar ???
+        reader = csv.reader(source)
+        writer = csv.writer(dest)
+
+        # first copy the (augmented) headers
+        headers = reader.next()
+        headers.append("list")
+        writer.writerow(headers)
+
+        # then loop on the content
+        for rownum, row in enumerate(reader):
+        
+            if row[1] in falselist:
+                result = ["[0, 1]"]
+            else:
+                result =["[1, 0]"]
+
+            row.append(result)
+            writer.writerow(row)
